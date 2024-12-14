5bf4fc64a5f5badd3bf1037e3fee0337608bcdd5
Dale de Silva <daledesilva@gmail.com>
2024-12-15 01:21:09 +1100

    Added masic mixpanel setup

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,17 @@ import React, { FC, ReactNode, useEffect } from 'react';
 import ActionButton from './components/ActionButton';
 import { ObjectText, DestText, TriggerText, FocusText } from './components/StyledText';
 import { atom, useAtom, useAtomValue } from 'jotai'
+import mixpanel from "mixpanel-browser";
 
 ///////////
+///////////
+
+mixpanel.init(process.env.MIXPANEL_TOKEN, {
+    debug: true,
+    track_pageview: true,
+    persistence: "localStorage",
+});
+
 ///////////
 
 export type UserSelection = 'none' | 'boards';
