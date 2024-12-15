7a363f78295b526a41043fff447bfdfddfff2718
Dale de Silva <daledesilva@gmail.com>
2024-12-15 12:26:26 +1100

    Added open source license

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
 import "./style.css";
 
+//////////////////////////////////
+//////////////////////////////////
+
+// Copyright (C) 2024  Dale de Silva
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//////////////////////////////////
+//////////////////////////////////
+
 // get the current theme from the URL
 const searchParams = new URLSearchParams(window.location.search);
 document.body.dataset.theme = searchParams.get("theme") ?? "light";
